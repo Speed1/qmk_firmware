764cf9597b21c5b923b571f7826d8a801db6226d
aspitaler <alexander.spitaler@eurac.edu>
2025-01-17 09:01:13 +0100

    add double press for umlaute, cleanup format

--- keyboards/keycapsss/3w6_2040/keymaps/speed1/keymap.c
+++ keyboards/keycapsss/3w6_2040/keymaps/speed1/keymap.c
@@ -28,6 +28,7 @@ enum custom_keycodes {
     CKC_SPC,
     CKC_TAB,
     CKC_ENT,
+    CKC_U,
     SMTD_KEYCODES_END,
 };
 
@@ -37,23 +38,104 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_smtd(keycode, record)) {
         return false;
     }
-   return true;
+    return true;
 }
 
 void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
     switch (keycode) {
-        SMTD_MT(CKC_A, KC_A, KC_LEFT_GUI)
+        // SMTD_MT(CKC_A, KC_A, KC_LEFT_GUI)
         SMTD_MT(CKC_R, KC_R, KC_LEFT_ALT)
-        SMTD_MT(CKC_S, KC_S, KC_LEFT_CTRL)      
+        SMTD_MT(CKC_S, KC_S, KC_LEFT_CTRL)
         SMTD_MT(CKC_T, KC_T, KC_LSFT)
         SMTD_MT(CKC_N, KC_N, KC_RSFT)
         SMTD_MT(CKC_E, KC_E, KC_RIGHT_CTRL)
         SMTD_MT(CKC_I, KC_I, KC_RIGHT_ALT)
-        SMTD_MT(CKC_O, KC_O, KC_RIGHT_GUI)
+        //SMTD_MT(CKC_O, KC_O, KC_RIGHT_GUI)
         SMTD_LT(CKC_ESC, KC_ESC, _CFG)
         SMTD_LT(CKC_SPC, KC_SPC, _NUM)
         SMTD_LT(CKC_TAB, KC_TAB, _NAV)
         SMTD_LT(CKC_ENT, KC_ENT, _SYM)
+
+        case CKC_A: {
+            switch (action) {
+                case SMTD_ACTION_TOUCH:
+                    break;
+                case SMTD_ACTION_TAP:
+                    if (tap_count < 1) {
+                        tap_code16(is_caps_word_on() ? LSFT(KC_A) : KC_A);
+                    } else {
+                        tap_code16(KC_BSPC); // to delete previous KC_A
+                        tap_code16(is_caps_word_on() ? LSFT(KC_QUOT) : KC_QUOT);
+                    }
+                    break;
+                case SMTD_ACTION_HOLD:
+                    if (tap_count < 1) {
+                        register_mods(MOD_BIT(KC_LEFT_GUI));
+                    } else {
+                        register_code16(is_caps_word_on() ? LSFT(KC_A) : KC_A);
+                    }
+                    break;
+                case SMTD_ACTION_RELEASE:
+                    if (tap_count < 1) {
+                        unregister_mods(MOD_BIT(KC_LEFT_GUI));
+                    } else {
+                        unregister_code16(is_caps_word_on() ? LSFT(KC_A) : KC_A);
+                        send_keyboard_report();
+                    }
+                    break;
+            }
+            break;
+        }
+
+        case CKC_O: {
+            switch (action) {
+                case SMTD_ACTION_TOUCH:
+                    break;
+                case SMTD_ACTION_TAP:
+                    if (tap_count < 1) {
+                        tap_code16(is_caps_word_on() ? LSFT(KC_O) : KC_O);
+                    } else {
+                        tap_code16(KC_BSPC); // to delete previous KC_A
+                        tap_code16(is_caps_word_on() ? LSFT(KC_SCLN) : KC_SCLN);
+                    }
+                    break;
+                case SMTD_ACTION_HOLD:
+                    if (tap_count < 1) {
+                        register_mods(MOD_BIT(KC_RIGHT_GUI));
+                    } else {
+                        register_code16(is_caps_word_on() ? LSFT(KC_O) : KC_O);
+                    }
+                    break;
+                case SMTD_ACTION_RELEASE:
+                    if (tap_count < 1) {
+                        unregister_mods(MOD_BIT(KC_RIGHT_GUI));
+                    } else {
+                        unregister_code16(is_caps_word_on() ? LSFT(KC_O) : KC_O);
+                        send_keyboard_report();
+                    }
+                    break;
+            }
+            break;
+        }
+        case CKC_U: {
+            switch (action) {
+                case SMTD_ACTION_TOUCH:
+                    break;
+                case SMTD_ACTION_TAP:
+                    if (tap_count < 1) {
+                        tap_code16(is_caps_word_on() ? LSFT(KC_U) : KC_U);
+                    } else {
+                        tap_code16(KC_BSPC); // to delete previous KC_A
+                        tap_code16(is_caps_word_on() ? LSFT(KC_LBRC) : KC_LBRC);
+                    }
+                    break;
+                case SMTD_ACTION_HOLD:
+                break;
+                case SMTD_ACTION_RELEASE:
+                break;
+            }
+            break;
+        }
     }
 }
 
@@ -62,9 +144,9 @@ void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
 // start Combos
 
 const uint16_t PROGMEM lock_device[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM close_app[] = {KC_Q, KC_P, COMBO_END};
-const uint16_t PROGMEM caps_lock[] = {CKC_T, CKC_N, COMBO_END};
-combo_t key_combos[] = {
+const uint16_t PROGMEM close_app[]   = {KC_Q, KC_P, COMBO_END};
+const uint16_t PROGMEM caps_lock[]   = {CKC_T, CKC_N, COMBO_END};
+combo_t key_combos[]  = {
     COMBO(lock_device, C(G(KC_Q))),
     COMBO(close_app, G(KC_Q)),
     COMBO(caps_lock, KC_CAPS),
@@ -84,10 +166,10 @@ enum {
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
-    [TD_C_copy] = ACTION_TAP_DANCE_DOUBLE(KC_C, RGUI(KC_C)),
+    [TD_C_copy]  = ACTION_TAP_DANCE_DOUBLE(KC_C, RGUI(KC_C)),
     [TD_V_paste] = ACTION_TAP_DANCE_DOUBLE(KC_V, RGUI(KC_V)),
-    [TD_X_cut] = ACTION_TAP_DANCE_DOUBLE(KC_X, RGUI(KC_X)),
-    [TD_Y_undo] = ACTION_TAP_DANCE_DOUBLE(KC_Y, RGUI(KC_Y)),
+    [TD_X_cut]   = ACTION_TAP_DANCE_DOUBLE(KC_X, RGUI(KC_X)),
+    [TD_Y_undo]  = ACTION_TAP_DANCE_DOUBLE(KC_Y, RGUI(KC_Y)),
 };
 
 // end Tap Dance declarations
@@ -97,40 +179,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
 
     [_ALPHA_COLEMAK] = LAYOUT_split_3x5_3(
-        KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,                                                                KC_J,    KC_L,    KC_U,    KC_Z,    KC_MINS,
-        CKC_A, CKC_R, CKC_S, CKC_T, KC_G,                                                                        KC_M, CKC_N, CKC_E, CKC_I, CKC_O,
-        TD(TD_Y_undo), TD(TD_X_cut), TD(TD_C_copy), KC_D, TD(TD_V_paste),                             KC_K,    KC_H,    KC_COMMA, KC_DOT,  KC_SLASH,
-           CKC_ESC, CKC_SPC, CKC_TAB,                                                                        KC_BSPC, CKC_ENT, KC_DEL
+        KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,                                          KC_J,    KC_L,    CKC_U,    KC_Z,    KC_MINS,
+        CKC_A, CKC_R, CKC_S, CKC_T, KC_G,                                                  KC_M, CKC_N, CKC_E, CKC_I, CKC_O,
+        TD(TD_Y_undo), TD(TD_X_cut), TD(TD_C_copy), KC_D, TD(TD_V_paste),       KC_K,    KC_H,    KC_COMMA, KC_DOT,  KC_SLASH,
+                CKC_ESC, CKC_SPC, CKC_TAB,                                                      KC_BSPC, CKC_ENT, KC_DEL
     ),
     [_ALPHA_QWERTY] = LAYOUT_split_3x5_3(
-        KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                      KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
-        KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                                                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-            LT(_CFG,KC_ESCAPE), LT(_NUM,KC_SPC), LT(_NAV, KC_TAB),                     KC_BSPC, LT(_SYM, KC_ENT), KC_DEL
+        KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
+        KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+             LT(_CFG,KC_ESCAPE), LT(_NUM,KC_SPC), LT(_NAV, KC_TAB),                     KC_BSPC, LT(_SYM, KC_ENT), KC_DEL
     ),
     [_SYM] = LAYOUT_split_3x5_3(
         RALT(KC_8) , LSFT(KC_6),   LSFT(KC_RBRC),  LSFT(KC_2), RALT(KC_9),            RSFT(KC_EQUAL), KC_NONUS_BACKSLASH, KC_GRAVE, RSFT(KC_GRAVE), XXXXXXX,
         LSFT(KC_DOT), LSFT(KC_4), RSFT(KC_5), LSFT(KC_7),  KC_RBRC,                    RSFT(KC_NONUS_HASH), RALT_T(KC_N), XXXXXXX, XXXXXXX, XXXXXXX,
-        LSFT(KC_MINUS), LSFT(KC_1), RALT(KC_L), KC_NONUS_HASH, RALT(KC_7),                KC_EQUAL, XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,
-          LSFT(KC_8), LSFT(KC_9), LSFT(KC_SLASH),                                          XXXXXXX, XXXXXXX, XXXXXXX
+        LSFT(KC_MINUS), LSFT(KC_1), RALT(KC_L), KC_NONUS_HASH, RALT(KC_7),          KC_EQUAL, XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,
+                LSFT(KC_8), LSFT(KC_9), LSFT(KC_SLASH),                                           XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_NAV] = LAYOUT_split_3x5_3(
-        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                XXXXXXX, RGUI(KC_V),  RGUI(KC_C), RGUI(KC_X), RGUI(KC_Y),
-        KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT, XXXXXXX,                                KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                                        XXXXXXX, XXXXXXX, XXXXXXX,               XXXXXXX, MO(_CFG), XXXXXXX
+        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,          XXXXXXX, RGUI(KC_V),  RGUI(KC_C), RGUI(KC_X), RGUI(KC_Y),
+        KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT, XXXXXXX,          KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+                  XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, MO(_CFG), XXXXXXX
     ),
      [_NUM] = LAYOUT_split_3x5_3(
-        XXXXXXX,  KC_F7, KC_F8, KC_F9, KC_F10,                                       RALT(KC_5),  KC_7,  KC_8,  KC_9,  RALT(KC_6),
-        RGUI(KC_A),  KC_F4,  KC_F5,  KC_F6,  KC_F11,                                    RSFT(KC_0),  KC_4,  KC_5,  KC_6, RSFT(KC_COMMA),
-        XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F12,                                    S(A(DE_7)),  KC_1,  KC_2,  KC_3, KC_DOT,
-                                        XXXXXXX, XXXXXXX, XXXXXXX,                RSFT(KC_0), KC_0, KC_PENT
+        XXXXXXX,  KC_F7, KC_F8, KC_F9, KC_F10,                    RALT(KC_5),  KC_7,  KC_8,  KC_9,  RALT(KC_6),
+        RGUI(KC_A),  KC_F4,  KC_F5,  KC_F6,  KC_F11,             RSFT(KC_0),  KC_4,  KC_5,  KC_6, RSFT(KC_COMMA),
+        XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F12,                 S(A(DE_7)),  KC_1,  KC_2,  KC_3, KC_DOT,
+                  XXXXXXX, XXXXXXX, XXXXXXX,                        RSFT(KC_0), KC_0, KC_PENT
     ),
         [_CFG] = LAYOUT_split_3x5_3(
-        RGUI(KC_Y), RGUI(KC_X), RGUI(KC_C), RGUI(KC_V), XXXXXXX,                                XXXXXXX, XXXXXXX, KC_LBRC,DF(_ALPHA_COLEMAK), DF(_ALPHA_QWERTY),
-        KC_QUOT, XXXXXXX, XXXXXXX, LSFT_T(XXXXXXX), XXXXXXX,                     XXXXXXX, RSFT_T(XXXXXXX), XXXXXXX, XXXXXXX, KC_SCLN,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                         XXXXXXX, XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX, QK_BOOT
+        RGUI(KC_Y), RGUI(KC_X), RGUI(KC_C), RGUI(KC_V), XXXXXXX,          XXXXXXX, XXXXXXX, KC_LBRC,DF(_ALPHA_COLEMAK), DF(_ALPHA_QWERTY),
+        KC_QUOT, XXXXXXX, XXXXXXX, LSFT_T(XXXXXXX), XXXXXXX,              XXXXXXX, RSFT_T(XXXXXXX), XXXXXXX, XXXXXXX, KC_SCLN,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                XXXXXXX, XXXXXXX, XXXXXXX,                                            XXXXXXX, XXXXXXX, QK_BOOT
     )
     // clang-format on
 };
