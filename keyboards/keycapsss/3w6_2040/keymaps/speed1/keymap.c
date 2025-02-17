b6327c1cd55cb7b9d546aff930c5f897b62d83c7
aspitaler <alexander.spitaler@eurac.edu>
2025-02-17 13:44:08 +0100

    cleanup, add screenshot combo

--- keyboards/keycapsss/3w6_2040/keymaps/speed1/keymap.c
+++ keyboards/keycapsss/3w6_2040/keymaps/speed1/keymap.c
@@ -28,7 +28,6 @@ enum custom_keycodes {
     CKC_SPC,
     CKC_TAB,
     CKC_ENT,
-    CKC_U,
     SMTD_KEYCODES_END,
 };
 
@@ -71,6 +70,7 @@ const uint16_t PROGMEM TD_OE[] = {CKC_I, CKC_O, COMBO_END};
 const uint16_t PROGMEM TD_UE[] = {KC_U, KC_Z, COMBO_END};
 const uint16_t PROGMEM TD_ENT[] = {CKC_S, CKC_T, COMBO_END};
 const uint16_t PROGMEM TD_BSPC[] = {KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM TD_SCREENSHOT[] = {KC_C, KC_D, COMBO_END};
 combo_t key_combos[]  = {
     COMBO(lock_device, LCTL(RGUI(KC_Q))),
     COMBO(close_app, RGUI(KC_Q)),
@@ -81,6 +81,7 @@ combo_t key_combos[]  = {
     COMBO(TD_UE, KC_LBRC),
     COMBO(TD_ENT, KC_ENT),
     COMBO(TD_BSPC, KC_BSPC),
+    COMBO(TD_SCREENSHOT, LGUI(LSFT(KC_4))),
 };
 
 // end Combos
@@ -130,40 +131,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
 
     [_ALPHA_COLEMAK] = LAYOUT_split_3x5_3(
-        KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,                                          KC_J,    KC_L,    KC_U,    KC_Z,    KC_MINS,
-        CKC_A, CKC_R, CKC_S, CKC_T, KC_G,                                                  KC_M, CKC_N, CKC_E, CKC_I, CKC_O,
-        TD(TD_Y_undo), KC_X, KC_C, KC_D, KC_V,                                           KC_K,    KC_H,    KC_COMMA, KC_DOT,  KC_SLASH,
-                CKC_ESC, CKC_SPC, CKC_TAB,                                                      KC_BSPC, CKC_ENT, KC_DEL
+        KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,                                                             KC_J,    KC_L,    KC_U,    KC_Z,    KC_MINS,
+        CKC_A, CKC_R, CKC_S, CKC_T, KC_G,                                                                     KC_M, CKC_N, CKC_E, CKC_I, CKC_O,
+        TD(TD_Y_undo), KC_X, KC_C, KC_D, KC_V,                                                              KC_K,    KC_H,    KC_COMMA, KC_DOT,  KC_SLASH,
+                CKC_ESC, CKC_SPC, CKC_TAB,                                                                         KC_BSPC, CKC_ENT, KC_DEL
     ),
     [_ALPHA_QWERTY] = LAYOUT_split_3x5_3(
-        KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
-        KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-             LT(_CFG,KC_ESCAPE), LT(_NUM,KC_SPC), LT(_NAV, KC_TAB),                     KC_BSPC, LT(_SYM, KC_ENT), KC_DEL
+        KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                                KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
+        KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                                                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+             LT(_CFG,KC_ESCAPE), LT(_NUM,KC_SPC), LT(_NAV, KC_TAB),                                      KC_BSPC, LT(_SYM, KC_ENT), KC_DEL
     ),
     [_SYM] = LAYOUT_split_3x5_3(
-        RALT(KC_8) , LSFT(KC_6),   LSFT(KC_RBRC),  LSFT(KC_2), RALT(KC_9),            RSFT(KC_EQUAL), KC_NONUS_BACKSLASH, KC_GRAVE, RSFT(KC_GRAVE), XXXXXXX,
-        LSFT(KC_DOT), LSFT(KC_4), RSFT(KC_5), LSFT(KC_7),  KC_RBRC,                    RSFT(KC_NONUS_HASH), LALT(DE_N), XXXXXXX, XXXXXXX, XXXXXXX,
-        LSFT(KC_MINUS), LSFT(KC_1), RALT(KC_L), KC_NONUS_HASH, RALT(KC_7),          KC_EQUAL, XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,
-                LSFT(KC_8), LSFT(KC_9), LSFT(KC_SLASH),                                           XXXXXXX, XXXXXXX, XXXXXXX
+        RALT(KC_8) , LSFT(KC_6),   LSFT(KC_RBRC),  LSFT(KC_2), RALT(KC_9),                          RSFT(KC_EQUAL), KC_NONUS_BACKSLASH, KC_GRAVE, RSFT(KC_GRAVE), XXXXXXX,
+        LSFT(KC_DOT), LSFT(KC_4), RSFT(KC_5), LSFT(KC_7),  KC_RBRC,                                   RSFT(KC_NONUS_HASH), LALT(DE_N), XXXXXXX, XXXXXXX, XXXXXXX,
+        LSFT(KC_MINUS), LSFT(KC_1), RALT(KC_L), KC_NONUS_HASH, RALT(KC_7),                         KC_EQUAL, XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,
+                LSFT(KC_8), LSFT(KC_9), LSFT(KC_SLASH),                                                       XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_NAV] = LAYOUT_split_3x5_3(
-        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                            XXXXXXX, RGUI(KC_V),  RGUI(KC_C), RGUI(KC_X), RGUI(KC_Y),
-        KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT, XXXXXXX,                           KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-        RGUI(KC_Y), RGUI(KC_X), RGUI(KC_C), RGUI(KC_V), XXXXXXX,              XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                  XXXXXXX, XXXXXXX, XXXXXXX,                                                XXXXXXX, MO(_CFG), XXXXXXX
+        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                                       XXXXXXX, RGUI(KC_V),  RGUI(KC_C), RGUI(KC_X), RGUI(KC_Y),
+        LGUI_T(KC_MPRV), LALT_T(KC_MPLY), LCTL_T(KC_MPLY), LSFT_T(KC_MNXT), XXXXXXX,           KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
+        RGUI(KC_Y), RGUI(KC_X), RGUI(KC_C), RGUI(KC_V), XXXXXXX,                                         XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+                  XXXXXXX, XXXXXXX, XXXXXXX,                                                                         XXXXXXX, MO(_CFG), XXXXXXX
     ),
      [_NUM] = LAYOUT_split_3x5_3(
-        XXXXXXX,  KC_F7, KC_F8, KC_F9, KC_F10,                    RALT(KC_5),  KC_7,  KC_8,  KC_9,  RALT(KC_6),
-        RGUI(KC_A),  KC_F4,  KC_F5,  KC_F6,  KC_F11,             RSFT(KC_0),  KC_4,  KC_5,  KC_6, RSFT(KC_COMMA),
-        XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F12,                 RSFT(RALT(DE_7)),  KC_1,  KC_2,  KC_3, KC_DOT,
-                  XXXXXXX, XXXXXXX, XXXXXXX,                        RSFT(KC_0), KC_0, KC_PENT
+        XXXXXXX,  KC_F7, KC_F8, KC_F9, KC_F10,                                                                RALT(KC_5),  KC_7,  KC_8,  KC_9,  RALT(KC_6),
+        RGUI(KC_A), LALT_T(KC_F4),  KC_F5,  KC_F6,  KC_F11,                                                RSFT(KC_0),  KC_4,  KC_5,  KC_6, RSFT(KC_COMMA),
+        XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F12,                                                             RSFT(RALT(DE_7)),  KC_1,  KC_2,  KC_3, KC_DOT,
+                  XXXXXXX, XXXXXXX, XXXXXXX,                                                                          KC_BSPC, KC_0, KC_PENT
     ),
         [_CFG] = LAYOUT_split_3x5_3(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_LBRC,DF(_ALPHA_COLEMAK), DF(_ALPHA_QWERTY),
-        KC_QUOT, XXXXXXX, XXXXXXX, LSFT_T(XXXXXXX), XXXXXXX,              XXXXXXX, RSFT_T(XXXXXXX), XXXXXXX, XXXXXXX, KC_SCLN,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                XXXXXXX, XXXXXXX, XXXXXXX,                                            XXXXXXX, XXXXXXX, QK_BOOT
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                        XXXXXXX, XXXXXXX, KC_LBRC,DF(_ALPHA_COLEMAK), DF(_ALPHA_QWERTY),
+        KC_QUOT, XXXXXXX, XXXXXXX, LSFT_T(XXXXXXX), XXXXXXX,                                             XXXXXXX, RSFT_T(XXXXXXX), XXXXXXX, XXXXXXX, KC_SCLN,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                XXXXXXX, XXXXXXX, XXXXXXX,                                                                          XXXXXXX, XXXXXXX, QK_BOOT
     )
     // clang-format on
 };
