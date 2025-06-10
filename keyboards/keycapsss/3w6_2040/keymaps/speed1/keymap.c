3b73cb3e16d3888e9de681ab4efbce78616acd62
Speed1 <alexander.spitaler@outlook.com>
2025-06-10 13:42:42 +0200

    changes

--- keyboards/keycapsss/3w6_2040/keymaps/speed1/keymap.c
+++ keyboards/keycapsss/3w6_2040/keymaps/speed1/keymap.c
@@ -264,7 +264,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ALPHA_COLEMAK] = LAYOUT_split_3x5_3(
         KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,                                                             KC_J,    KC_L,    KC_U,    KC_Z,    KC_MINS,
-        KC_A, CKC_R, KC_S, KC_T, KC_G,                                                                     KC_M, KC_N, KC_E, CKC_I, KC_O,
+        KC_A, CKC_R, KC_S, KC_T, KC_G,                                                                         KC_M, KC_N, KC_E, CKC_I, KC_O,
         KC_Y, KC_X, KC_C, KC_D, KC_V,                                                                           KC_K,    KC_H,    KC_COMMA, KC_DOT,  KC_SLASH,
                 KC_ESC, KC_SPC, KC_TAB,                                                                         KC_BSPC, KC_ENT, KC_DEL
     ),
